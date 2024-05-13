7204fe13d688b7c497b8db0162b793f53e0cc84e
kamehamehaa0000 <gayush794@gmail.com>
2024-05-13 19:30:36 +0530

    smsgs

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -7,7 +7,7 @@ const __dirname = dirname(__filename)
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    const destinationPath = join(__dirname, 'public', 'temp') // Constructing the destination path using path module
+    const destinationPath = join(__dirname, '..', 'public', 'temp') // Adjusting the path to go up one directory to the project root
     callback(null, destinationPath)
   },
   filename: function (req, file, callback) {
